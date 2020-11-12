1b1408fa2da9d8442b759936fb3d8b08c7a210a3
Diegosny <diego.camilo@versatecnologia.com.br>
2020-11-12 09:08:03 -0300

    Adicionando Listagem de Usuario

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -7,41 +7,27 @@
 @section('content')
     <div class="container-fluid">
         <div class="card shadow">
-            <div class="card-body">
-                <div class="modal fade" id="createModal" tabindex="-1" role="dialog">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title">Criar Equipes</h5>
-                                <button type=" button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div style="margin-left: 16px; margin-right: 16px;">
-                                    <form method="post" class="user" action="/equipes/add">
-                                        @csrf
-                                        <div class="row">
-                                            <label class="">Nome:</label>
-                                            <input style="margin-left: 5px;" class="form-control form-control-user"
-                                                   type="text">
-                                        </div>
-                                        <div style="margin-top: 10px" class="row">
-                                            <label>Meta:</label>
-                                            <input style="margin-left: 5px;" class="form-control form-control-user  "
-                                                   type="number">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Save changes</button>
-                                    </form>
-
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                            </div>
-
-                        </div>
-                    </div>
+              <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Ação</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($usuarios as $usuario)        
+                        <tr>
+                            <th scope="row"> {{ $usuario->id}} </th>
+                            <td> {{ $usuario->name}} </td>
+                            <td> {{ $usuario->email}} </td>
+                            <td> Editar </td>
+                        </tr>
+                      @endforeach
+    
+                    </tbody>
+                  </table>
                 </div>
             </div>
         </div>
